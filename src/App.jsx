4e839974b8b15c18d865eb7e85c0869562a01fa9
Gaurav Choudhary <gloonaich@gmail.com>
2023-06-07 20:59:32 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import { useEffect } from "react";
 import "./App.css";
 
 // 80543ffc
 
+const API_URL = "http://www.omdbapi.com?apikey=80543ffc";
+
 function App() {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
   return <></>;
 }
 
